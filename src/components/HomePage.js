13b21f1b1d078f6865f16259cd7ac83961e48cf3
tigerweirdo <temmuzcetiner@gmail.com>
2023-04-13 08:27:34 +0200

    Bitti.

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Header from './Header';
+import './HomePage.css';
 
 const HomePage = () => {
   return (
     <div>
-      <h1>Anasayfa</h1>
-      <Link to="/pizza">
-        <button id="order-pizza">Sipariş Ver</button>
-      </Link>
+      <Header />
+      <div
+        className="banner-container"
+        style={{
+          backgroundImage: `url('Assets/mvp-banner.png')`,
+        }}
+      >
+        <h2 className="banner-text">
+          Kod acıktırır
+          <br />
+          Pizza doyurur
+        </h2>
+        <Link to="/pizza" style={{ textDecoration: 'none' }}>
+          <button
+            id="order-pizza"
+            className="order-pizza"
+            onMouseOver={(e) => (e.target.style.backgroundColor = '#FFC107')}
+            onMouseOut={(e) => (e.target.style.backgroundColor = '#F7D154')}
+          >
+            Acıktım
+          </button>
+        </Link>
+      </div>
     </div>
   );
-}
+};
 
 export default HomePage;
